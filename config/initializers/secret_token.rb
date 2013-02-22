147aaca45cb9a0a578a6c28ad7423357503648ef
Mathijs Blokland <mathijs.blokland@eduhub.nl>
2013-02-22 15:34:08 +0100

    initial tth commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendbook::Application.config.secret_token = 'dfdcb7ad2ef9b6bfe7605e8b45383ba06e6854e17b769c3b84e90bbc7ccd55a27d35093daaaf82c52eca9b5d1c7f282c43534611640f3225fd88851dd850fb9f'
